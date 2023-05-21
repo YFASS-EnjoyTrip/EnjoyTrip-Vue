85b8ec1ad788ed7d1371ed496fbad63464b22dd2
97Kzone <76652908+97Kzone@users.noreply.github.com>
2023-05-21 22:56:07 +0900

    feat. 마이페이지 기능 전체 구현 - 회원 정보 수정 기능 테스트 - 플래너 리스트 조회 - 좋아요한 여행지 조회

--- src/components/member/mypageComponents/MyInfo.vue
+++ src/components/member/mypageComponents/MyInfo.vue
@@ -4,25 +4,26 @@
       <span>정보 수정하기</span>
     </div>
     <div class="profile-img">
-      <img src="../../../assets/img/icon/profile_default.png" alt="">
+      <img :src="this.user.profileImg || defaultImg" alt="" draggable="false" />
     </div>
     <div class="modify-img">
       <span>사진 수정하기</span>
-      <font-awesome-icon class="modify-icon" icon="fa-pen-to-square" size="xl" />
+      <input type="file" accept=".png, .jpg, .jpeg" @change="onFileSelected" ref="fileInput" style="display: none" />
+      <font-awesome-icon class="modify-icon" icon="fa-pen-to-square" size="xl" @click="clickedModifyImg" />
     </div>
-    
+
     <div class="modify-bio">
-      <input type="text" v-model="modifyBio" :placeholder="bio">
-      <font-awesome-icon class="modify-icon" icon="fa-pen-to-square" size="xl" />
+      <input type="text" v-model="modifyBio" :placeholder="this.user.bio" />
+      <font-awesome-icon class="modify-icon" icon="fa-pen-to-square" size="xl" @click="clickedModifyBio" />
     </div>
     <div class="input-container">
       <div class="member-input">
-        <span>닉네임 </span>
-        <input class="input-box" type="text" v-model="nickname" @keyup="debouncedCheckNicknameDuplicate" />
+        <span>닉네임</span>
+        <input class="input-box" type="text" :value="user.nickname" @keyup="debouncedCheckNicknameDuplicate" readonly />
       </div>
       <span class="check">{{ nicknameMessage }}</span>
       <div class="member-input">
-        <span>비밀번호 </span>
+        <span>비밀번호</span>
         <input type="password" class="input-box" v-model="password" />
       </div>
       <span class="check password" v-show="password.length <= 3">비밀번호는 4자 이상이어야 합니다!</span>
@@ -41,49 +42,61 @@
 </template>
 
 <script>
-import { apiInstance } from "@/api/index.js";
-import router from "@/router";
-import _ from "lodash";
+import { apiInstance } from '@/api/index.js';
+import { apiAuthInstance } from '@/api/index.js';
+import router from '@/router';
+import _ from 'lodash';
+import { mapGetters } from 'vuex';
 
+const memberStore = 'memberStore';
 const api = apiInstance();
+const apiAuth = apiAuthInstance();
 
 export default {
-  name: "SignupForm",
+  name: 'SignupForm',
   components: {},
   data() {
     return {
-      nickname: "",
-      password: "",
-      confirmPassword: "",
+      nickname: '',
+      password: '',
+      confirmPassword: '',
       nicknameDuplicate: true,
-      bio:"기존 한줄 상태 입니다",
-      modifyBio:"",
+      bio: '기존 한줄 상태 입니다',
+      modifyBio: '',
+      defaultImg: 'https://enjoytrip-file-storage.s3.ap-northeast-2.amazonaws.com/profile_default.png',
     };
   },
   computed: {
     nicknameMessage() {
       return this.nickname.length == 0
-        ? ""
+        ? ''
         : this.nicknameDuplicate
-          ? "이미 사용중인 닉네임입니다."
-          : "사용이 가능한 닉네임입니다.";
+        ? '이미 사용중인 닉네임입니다.'
+        : '사용이 가능한 닉네임입니다.';
     },
     passwordMessage() {
-      return this.password !== this.confirmPassword &&
-        this.confirmPassword.length > 0
-        ? "비밀번호가 일치하지 않습니다."
-        : "비밀번호가 일치합니다.";
+      return this.password !== this.confirmPassword && this.confirmPassword.length > 0
+        ? '비밀번호가 일치하지 않습니다.'
+        : '비밀번호가 일치합니다.';
+    },
+
+    ...mapGetters(memberStore, ['checkUserInfo']),
+    user() {
+      return (
+        this.checkUserInfo || {
+          nickname: '',
+          profileImg: '',
+          bio: '',
+        }
+      );
     },
   },
+
   created() {
-    this.debouncedCheckNicknameDuplicate = _.debounce(
-      this.checkNicknameDuplicate,
-      500
-    );
+    this.debouncedCheckNicknameDuplicate = _.debounce(this.checkNicknameDuplicate, 500);
   },
 
   methods: {
-
     checkNicknameDuplicate() {
       api
         .get(`/member/check/${this.nickname}`)
@@ -101,52 +114,92 @@ export default {
 
     submitForm() {
       api
-        .post("/member/signup", {
+        .post('/member/signup', {
           password: this.password,
           nickname: this.nickname,
         })
         .then(() => {
-          router.push("/login");
+          router.push('/login');
         })
         .catch((error) => {
-          console.error("Signup failed", error);
+          console.error('Signup failed', error);
         });
+    },
+
+    async clickedModifyBio() {
+      try {
+        const response = await apiAuth.put(`/member/mypage/edit/bio`, {
+          bio: this.modifyBio,
+        });
+
+        if (response.status === 200) {
+          this.$store.commit(`${memberStore}/SET_USER_BIO`, this.modifyBio);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
+
+    clickedModifyImg() {
+      this.$refs.fileInput.click();
+    },
+
+    async onFileSelected(event) {
+      try {
+        const selectedFile = event.target.files[0];
+        if (selectedFile) {
+          const formData = new FormData();
+          formData.append('file', selectedFile);
+
+          const response = await api.put(`/member/mypage/edit/profileimg`, formData, {
+            headers: {
+              'Content-Type': 'multipart/form-data',
+              Authorization: 'Bearer ' + sessionStorage.getItem('access-token'),
+            },
+          });
+
+          if (response.status === 200) {
+            this.$store.commit(`${memberStore}/SET_USER_IMAGE`, response.data.result);
+          }
+        }
+      } catch (error) {
+        console.log(error);
+      }
     },
   },
 };
 </script>
 
 <style scoped>
-
-.modify-icon{
+.modify-icon {
   color: #686868;
   margin-left: 10px;
 }
-.modify-icon:hover{
+.modify-icon:hover {
   color: #383838;
   cursor: pointer;
   transform: scale(1.2);
   transition: 0.2s;
 }
-.modify-bio input{
-    border: 3px solid #69beee;
+.modify-bio input {
+  border: 3px solid #69beee;
   border-radius: 8px;
   outline: none;
   font-size: 20px;
   text-indent: 10px;
-  font-family: "CookieRun-Regular";
+  font-family: 'CookieRun-Regular';
   color: #757575;
   width: 300px;
   height: 40px;
   caret-color: #6e6e6e;
 }
-.modify-bio{
+.modify-bio {
   margin-top: 20px;
   text-align: center;
 }
-.modify-img{
+.modify-img {
   margin-top: 10px;
-  font-family: "CookieRun-Regular";
+  font-family: 'CookieRun-Regular';
   font-size: 15px;
   color: #707070;
   margin-left: 250px;
@@ -175,7 +228,7 @@ export default {
   border-radius: 10px;
   width: 120px;
   height: 45px;
-  font-family: "CookieRun-Regular";
+  font-family: 'CookieRun-Regular';
   color: #ffffff;
   font-size: 24px;
 
@@ -201,7 +254,7 @@ export default {
   outline: none;
   font-size: 20px;
   text-indent: 10px;
-  font-family: "CookieRun-Regular";
+  font-family: 'CookieRun-Regular';
   color: #757575;
 
   margin-right: 20px;
@@ -216,7 +269,7 @@ export default {
 
 .check {
   margin-left: 30px;
-  font-family: "CookieRun-Regular";
+  font-family: 'CookieRun-Regular';
   font-size: 15px;
   color: #ff7878;
   caret-color: transparent;
@@ -234,7 +287,7 @@ export default {
   user-select: none;
   pointer-events: none;
 
-  font-family: "CookieRun-Regular";
+  font-family: 'CookieRun-Regular';
   color: #636363;
   font-size: 20px;
 }
@@ -264,13 +317,13 @@ export default {
   width: 600px;
   height: 800px;
   border-radius: 0px 90px 90px 0px;
-  background-color: #CBE6F6;
+  background-color: #cbe6f6;
 }
 
 .title-container {
   margin-top: 30px;
   margin-left: 40px;
-  font-family: "CookieRun-Bold";
+  font-family: 'CookieRun-Bold';
   color: #383838;
   font-size: 30px;
   margin-bottom: -30px;
--- src/components/member/mypageComponents/MyLiked.vue
+++ src/components/member/mypageComponents/MyLiked.vue
@@ -1,290 +1,313 @@
 <template>
-    <div class="right-container">
-        <div class="title-container">
-            <span>좋아요 한 여행지</span>
-        </div>
-        <div class="input-container">
-            <select class="sido-dropdown" name="sido" id="location" v-model="sidoOption">
-                <option v-for="sido in sidos" :value="sido.value" :key="sido.value">
-                    {{ sido.text }}
-                </option>
-            </select>
-            <select class="gugun-dropdown" name="gugun" id="location" v-model="gugunOption">
-                <option v-for="gugun in guguns" :value="gugun.value" :key="gugun.value">
-                    {{ gugun.text }}
-                </option>
-            </select>
-            <div class="search-button">
-                <span>검색</span>
-            </div>
-        </div>
-        <div class="attraction-outer-container">
-            <div class="attraction-container" v-for="(attraction, index) in attractions" :key="index">
-                <div class="img-heart">
-                    <img class="attractionImg" :src="attraction.image || defaultImage" :alt="attraction.title"
-                        @click="goToAttractionDetail(attraction)" />
-                </div>
-                <div class="attraction-info">
-                    <div class="attraction-title">
-                        <span>{{ attraction.title }}</span>
-                    </div>
-                    <div class="like-heart">
-                        <img class="icon" :src="
-                            attraction.isLike
-                                ? 'https://enjoytrip-file-storage.s3.ap-northeast-2.amazonaws.com/heart_fill.png'
-                                : 'https://enjoytrip-file-storage.s3.ap-northeast-2.amazonaws.com/heart_empty.png'
-                        " alt="하트" />
-                        <span>({{ attraction.likeCount }})</span>
-                        <img class="icon" src="../../../assets/img/icon/star_fill.png" alt="별" />
-                        <span>{{ attraction.rank }}</span>
-                        <span>({{ attraction.rankCnt }})</span>
-                    </div>
-                </div>
-            </div>
-        </div>
+  <div class="right-container">
+    <div class="title-container">
+      <span>좋아요 한 여행지</span>
     </div>
+    <div class="input-container">
+      <select class="sido-dropdown" name="sido" id="location" v-model="sidoOption">
+        <option v-for="sido in sidos" :value="sido.value" :key="sido.value">
+          {{ sido.text }}
+        </option>
+      </select>
+      <select class="gugun-dropdown" name="gugun" id="location" v-model="gugunOption">
+        <option v-for="gugun in guguns" :value="gugun.value" :key="gugun.value">
+          {{ gugun.text }}
+        </option>
+      </select>
+      <div class="search-button" @click="searchLikeList">
+        <span>검색</span>
+      </div>
+    </div>
+    <div class="attraction-outer-container">
+      <div class="attraction-container" v-for="(attraction, index) in attractions" :key="index">
+        <div class="img-heart">
+          <img
+            class="attractionImg"
+            :src="attraction.image || defaultImage"
+            :alt="attraction.title"
+            @click="goToAttractionDetail(attraction)"
+          />
+        </div>
+        <div class="attraction-info">
+          <div class="attraction-title">
+            <span>{{ attraction.title }}</span>
+          </div>
+          <div class="like-heart">
+            <img
+              class="icon"
+              src="https://enjoytrip-file-storage.s3.ap-northeast-2.amazonaws.com/heart_fill.png"
+              alt="하트"
+            />
+            <span>({{ attraction.likeCount }})</span>
+            <img class="icon" src="../../../assets/img/icon/star_fill.png" alt="별" />
+            <span>{{ attraction.rank }}</span>
+            <span>({{ attraction.rankCnt }})</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import axios from "axios";
+import { apiAuthInstance } from '@/api';
+import axios from 'axios';
+
+const api = apiAuthInstance();
 export default {
-    components: {},
-    data() {
-        return {
-            // 23.05.16 기본이미지, 페이지네이션 추가
-            defaultImage:
-                "https://enjoytrip-file-storage.s3.ap-northeast-2.amazonaws.com/Attraction_default.png",
-            page: 1,
-            pageSize: 100,
-            //
-            keyword: "",
-            sidoOption: "0",
-            gugunOption: "",
-            sidos: [
-                { value: "0", text: "전체" },
-                { value: "1", text: "서울" },
-                { value: "2", text: "인천" },
-                { value: "3", text: "대전" },
-                { value: "4", text: "대구" },
-                { value: "5", text: "광주" },
-                { value: "6", text: "부산" },
-                { value: "7", text: "울산" },
-                { value: "8", text: "세종" },
-                { value: "31", text: "경기도" },
-                { value: "32", text: "강원도" },
-                { value: "33", text: "충청북도" },
-                { value: "34", text: "충청남도" },
-                { value: "35", text: "경상북도" },
-                { value: "36", text: "경상남도" },
-                { value: "37", text: "전라북도" },
-                { value: "38", text: "전라남도" },
-                { value: "39", text: "제주도" },
-            ],
-            guguns: [],
-            attractions: [],
-        };
+  components: {},
+  data() {
+    return {
+      // 23.05.16 기본이미지, 페이지네이션 추가
+      defaultImage: 'https://enjoytrip-file-storage.s3.ap-northeast-2.amazonaws.com/Attraction_default.png',
+      page: 1,
+      pageSize: 100,
+      //
+      keyword: '',
+      sidoOption: '0',
+      gugunOption: '',
+      sidos: [
+        { value: '0', text: '전체' },
+        { value: '1', text: '서울' },
+        { value: '2', text: '인천' },
+        { value: '3', text: '대전' },
+        { value: '4', text: '대구' },
+        { value: '5', text: '광주' },
+        { value: '6', text: '부산' },
+        { value: '7', text: '울산' },
+        { value: '8', text: '세종' },
+        { value: '31', text: '경기도' },
+        { value: '32', text: '강원도' },
+        { value: '33', text: '충청북도' },
+        { value: '34', text: '충청남도' },
+        { value: '35', text: '경상북도' },
+        { value: '36', text: '경상남도' },
+        { value: '37', text: '전라북도' },
+        { value: '38', text: '전라남도' },
+        { value: '39', text: '제주도' },
+      ],
+      guguns: [],
+      attractions: [],
+    };
+  },
+  watch: {
+    sidoOption(newOption) {
+      this.updateGuguns(newOption);
     },
-    watch: {
-        sidoOption(newOption) {
-            this.updateGuguns(newOption);
-        },
-    },
-    async created() {
-        this.updateGuguns(this.sidoOption);
-        const response = await axios.get(
-            `http://localhost:8080/locations?page=1&pageSize=10`
-        );
+  },
+  async created() {
+    this.updateGuguns(this.sidoOption);
+    try {
+      const response = await api.get(`/member/mypage/like?sido=${this.sidoOption}&gugun=${this.gugunOption}`);
+
+      if (response.status === 200) {
+        console.log(response.data);
         this.attractions = response.data.result;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  methods: {
+    async updateGuguns(option) {
+      if (option == '0') {
+        this.guguns = [{ text: '전체', value: '0' }];
+        this.gugunOption = this.guguns[0].value;
+      } else {
+        const response = await axios.get(`http://localhost:8080/locations/search/gugun?sido=${option}`);
+        this.guguns = response.data.result;
+        this.gugunOption = this.guguns[0].value;
+      }
     },
-    methods: {
-        async updateGuguns(option) {
-            if (option == "0") {
-                this.guguns = [{ text: "전체", value: "0" }];
-                this.gugunOption = this.guguns[0].value;
-            } else {
-                const response = await axios.get(
-                    `http://localhost:8080/locations/search/gugun?sido=${option}`
-                );
-                this.guguns = response.data.result;
-                this.gugunOption = this.guguns[0].value;
-            }
-        },
-        goToAttractionDetail(attraction) {
-            sessionStorage.setItem("contentId", attraction.contentId);
-            sessionStorage.setItem("contentTitle", attraction.title);
-            this.$router.push({
-                name: "attractionDetail",
-            });
-        },
+    goToAttractionDetail(attraction) {
+      sessionStorage.setItem('contentId', attraction.contentId);
+      sessionStorage.setItem('contentTitle', attraction.title);
+      this.$router.push({
+        name: 'attractionDetail',
+      });
     },
+
+    async searchLikeList() {
+      try {
+        const response = await api.get(`/member/mypage/like?sido=${this.sidoOption}&gugun=${this.gugunOption}`);
+
+        if (response.status === 200) {
+          console.log(response.data);
+          this.attractions = response.data.result;
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
 };
 </script>
 
 <style scoped>
 .like-heart {
-    margin-top: 5px;
-    display: flex;
-    /* Flexbox 사용 */
-    align-items: center;
-    /* 수직 정렬: 요소들을 수직 중앙에 정렬 */
+  margin-top: 5px;
+  display: flex;
+  /* Flexbox 사용 */
+  align-items: center;
+  /* 수직 정렬: 요소들을 수직 중앙에 정렬 */
 }
 
 .like-heart span {
-    margin-right: 3px;
-    font-size: 13px;
-    /* 폰트 크기 설정 */
-    font-family: "CookieRun-Regular";
-    color: #757575;
+  margin-right: 3px;
+  font-size: 13px;
+  /* 폰트 크기 설정 */
+  font-family: 'CookieRun-Regular';
+  color: #757575;
 }
 
 .attraction-title span {
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    font-size: 18px;
-    /* 폰트 크기 설정 */
-    font-family: "CookieRun-Bold";
-    color: #4d4d4d;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  font-size: 18px;
+  /* 폰트 크기 설정 */
+  font-family: 'CookieRun-Bold';
+  color: #4d4d4d;
 }
 
 .attraction-title {
-    margin-top: 5px;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
+  margin-top: 5px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
 }
 
 .icon {
-    width: 17px;
-    margin-left: 3px;
-    margin-right: 2px;
+  width: 17px;
+  margin-left: 3px;
+  margin-right: 2px;
 }
 
 .attractionImg:hover {
-    transform: scale(1.1);
-    filter: brightness(60%);
-    transition: 0.2s;
+  transform: scale(1.1);
+  filter: brightness(60%);
+  transition: 0.2s;
 }
 
 .attractionImg:active {
-    transform: scale(0.9);
-    transition: 0.2s;
+  transform: scale(0.9);
+  transition: 0.2s;
 }
 
 .attractionImg {
-    width: 140px;
-    height: 100px;
-    border-radius: 10px;
-    box-shadow: 2px 4px 5px rgba(85, 85, 85, 0.5);
-    /* 그림자 스타일 및 값 설정 */
+  width: 140px;
+  height: 100px;
+  border-radius: 10px;
+  box-shadow: 2px 4px 5px rgba(85, 85, 85, 0.5);
+  /* 그림자 스타일 및 값 설정 */
 }
 
 .search-button {
-    display: flex;
-    justify-content: center;
-    width: 70px;
-    height: 30px;
-    border-radius: 5px;
-    background-color: #f24849;
-    font-family: "CookieRun-Bold";
-    font-size: 16px;
-    color: #ffffff;
-    cursor: pointer;
-    caret-color: transparent;
-    user-select: none;
-    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3);
+  display: flex;
+  justify-content: center;
+  width: 70px;
+  height: 30px;
+  border-radius: 5px;
+  background-color: #f24849;
+  font-family: 'CookieRun-Bold';
+  font-size: 16px;
+  color: #ffffff;
+  cursor: pointer;
+  caret-color: transparent;
+  user-select: none;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3);
 }
 
 .search-button:hover {
-    box-shadow: inset 0 2px 5px rgba(0, 0, 0, 0.3);
-    transition: 0.2s;
-    color: #d8d8d8;
-    background-color: #da4343;
+  box-shadow: inset 0 2px 5px rgba(0, 0, 0, 0.3);
+  transition: 0.2s;
+  color: #d8d8d8;
+  background-color: #da4343;
 }
 
 .search-button:active {
-    transform: scale(0.9);
-    transition: 0.2s;
+  transform: scale(0.9);
+  transition: 0.2s;
 }
 
-.search-button>span {
-    margin-top: 3px;
+.search-button > span {
+  margin-top: 3px;
 }
 
 .sido-dropdown {
-    border: 2px solid #c4c4c4;
-    border-radius: 8px;
-    outline: none;
-    font-size: 15px;
-    text-indent: 10px;
-    font-family: "CookieRun-Regular";
-    color: #757575;
+  border: 2px solid #c4c4c4;
+  border-radius: 8px;
+  outline: none;
+  font-size: 15px;
+  text-indent: 10px;
+  font-family: 'CookieRun-Regular';
+  color: #757575;
 
-    margin-right: 10px;
-    width: 100px;
+  margin-right: 10px;
+  width: 100px;
 
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.4);
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.4);
 }
 
 .gugun-dropdown {
-    border: 2px solid #c4c4c4;
-    border-radius: 8px;
-    outline: none;
-    font-size: 15px;
-    text-indent: 10px;
-    font-family: "CookieRun-Regular";
-    color: #757575;
+  border: 2px solid #c4c4c4;
+  border-radius: 8px;
+  outline: none;
+  font-size: 15px;
+  text-indent: 10px;
+  font-family: 'CookieRun-Regular';
+  color: #757575;
 
-    margin-right: 20px;
-    width: 130px;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.4);
+  margin-right: 20px;
+  width: 130px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.4);
 }
 
 .input-container {
-    margin-top: 50px;
-    display: flex;
-    justify-content: center;
+  margin-top: 50px;
+  display: flex;
+  justify-content: center;
 }
 
 .attraction-container {
-    margin-left: 10px;
-    margin-top: 30px;
-    margin-right: 10px;
-    caret-color: transparent;
-    user-select: none;
-    cursor: pointer;
-    width: 140px;
-    height: max-content;
+  margin-left: 10px;
+  margin-top: 30px;
+  margin-right: 10px;
+  caret-color: transparent;
+  user-select: none;
+  cursor: pointer;
+  width: 140px;
+  height: max-content;
 }
 
 .attraction-outer-container {
-    width: 540px;
-    height: 630px;
-    margin: auto;
-    margin-top: 20px;
-    border-radius: 18px;
-    overflow: scroll;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
+  width: 540px;
+  height: 630px;
+  margin: auto;
+  margin-top: 20px;
+  border-radius: 18px;
+  overflow: scroll;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
 }
 
 .right-container {
-    width: 600px;
-    height: 800px;
-    border-radius: 0px 90px 90px 0px;
-    background-color: #FFDEDE;
+  width: 600px;
+  height: 800px;
+  border-radius: 0px 90px 90px 0px;
+  background-color: #ffdede;
 }
 
 .title-container {
-    margin-top: 30px;
-    margin-left: 40px;
-    font-family: "CookieRun-Bold";
-    color: #383838;
-    font-size: 30px;
-    margin-bottom: -30px;
-    caret-color: transparent;
-    user-select: none;
-    pointer-events: none;
+  margin-top: 30px;
+  margin-left: 40px;
+  font-family: 'CookieRun-Bold';
+  color: #383838;
+  font-size: 30px;
+  margin-bottom: -30px;
+  caret-color: transparent;
+  user-select: none;
+  pointer-events: none;
 }
 </style>
